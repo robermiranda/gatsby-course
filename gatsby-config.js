8e03f4dbeb94a779b4cfa48b22abd2bce76739e4
Roberto Miranda <rob.mirandam@gmail.com>
2021-11-19 15:22:49 -0600

    Finished Gatsby tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby tutorial",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-import * as React from 'react'
+import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 
 const indexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby tutorial.</p>
+            <StaticImage
+                alt="Terminator"
+                src="https://c4.wallpaperflare.com/wallpaper/869/679/589/endoskeleton-t-800-terminator-genisys-wallpaper-preview.jpg"/>
         </Layout>
     )
 }
