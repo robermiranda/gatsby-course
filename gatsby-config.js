c225c7c3882a62beac5a25c57065c2f701b3a742
Roberto Miranda <rob.mirandam@gmail.com>
2021-11-19 18:11:25 -0600

    Finished Gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby tutorial",
+    title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: 'blog',
+        path: `${__dirname}/blog`
+      }
+    }
   ],
 };
